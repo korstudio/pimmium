1bf731694f2a4c32ad81faebd790104ed0d86692
Methas Tariya <korstudio@gmail.com>
2020-05-16 03:11:33 +0700

    Add web starter kit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// You can write a call and import your functions in this file.
+//
+// This file will be compiled into app.js
+// Feel free with using ES6 here.
+
+import dots from './modules/dots';
+
+(($) => {
+  // When DOM is ready
+  $(() => {
+    dots();
+  });
+})(jQuery);
---
+++ src/vendor_entries/vendor.js
@@ -0,0 +1,5 @@
+// All files in this file will be included into vendor.js file.
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.min.js',
+];
